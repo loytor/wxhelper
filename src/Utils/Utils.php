dd44aff4fcbfd91198bf68bd188d8b6885f0f4e0
yanbingti <loytor@sina.com>
2017-11-16 20:00:54 +0800

    增加获取 jsTicket接口

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -10,4 +10,32 @@ class Utils
         $sign = strtoupper(md5(urldecode(http_build_query($params)) . '&key=' . $key));
         return $sign;
     }
+
+    /**
+     * 对url进行签名，以便前端脚本使用
+     * @param string $url
+     * @param string $ticket
+     * @param string $type =jsapi/wxcard
+     * @return array
+     */
+    public function getTicketSignature($url, $ticket, $type = 'jsapi')
+    {
+        #去掉#号部分
+        $url = explode('#', $url);
+        $url = $url[0];
+
+        $timestamp = time();
+        $nonceStr = str_random(16);
+
+        // 这里参数的顺序要按照 key 值 ASCII 码升序排序
+        $string = "jsapi_ticket=$ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+        $signature = sha1($string);
+        $signPackage = array(
+            'appId' => $this->authorizer_appid,
+            'nonceStr' => $nonceStr,
+            'timestamp' => $timestamp,
+            'signature' => $signature,
+        );
+        return $signPackage;
+    }
 }

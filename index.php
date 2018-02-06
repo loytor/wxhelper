68093a1cd76863b55291cd07033b01bd89e11d69
yanbingti <loytor@sina.com>
2018-02-06 10:51:35 +0800

    debug

--- index.php
+++ index.php
@@ -1,21 +1,100 @@
-<?php
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <title>Search</title>
 
-require "vendor/autoload.php";
+    <style type="text/css">
+        table tr:nth-child(odd) {
+            background-color: lightgreen;
+        }
 
+        table td:nth-child(even) {
+            background-color: blue;
+        }
 
-$access_token = "adf5ZGU48DFy3x4u5ZHThd90oiZSIyKoBk1XifxF2tZ-EdUgTOCgP8__IbpkfvqsVmjI_d-JOdSFzv6dSJ4dBXsqbZRKfHFCx2zpssPJZ2uIIY8jZ-af9XXljnOwivk4JGGiAEAFCC";
-$component_appid = "wx0f56e0990602ec68";
+        table tr:hover {
+            background-color: gray !important;
+        }
 
-dd($access_token);
+        table td, table th {
+            border: 1px solid #EEE;
+        }
+    </style>
+</head>
 
-$component = new \Loytor\Wxhelper\Wechat\ApiCreatePreauthcode($component_appid, $access_token);
-$result = $component->createPreauthcode();
-var_dump($result);
+<body>
 
+<form action="1.php" method="POST">
+    username：<input type="text" name="username"/>
+    postname:<input type="text" name="postname"/>
+    postdate:<input type="text" name="postdate"/>
+    <br/>
+    <input type="submit"/>
+</form>
 
-function dd($result)
-{
-    echo '<pre>';
-    print_r($result);
-    die;
-}
+<div class="content">
+    <?php
+
+    define('DBHOST', 'localhost:8889');
+    define('DBNAME', 'test');
+    define('DBUSER', 'bj');
+    define('DBPASS', '1a2b3c');
+
+    //connect db
+    function connect_to_db()
+    {
+        try {
+            $connString = "mysql:host=" . DBHOST . ";dbname=" . DBNAME;
+            $user = DBUSER;
+            $password = DBPASS;
+            $pdo = new PDO($connString, $user, $password);
+            return $pdo;
+        } catch (PDOException $e) {
+            echo($e->getMessage());
+            exit('Connected failed');
+        }
+    }
+
+    #1.创建 pdo
+    $pdo = connect_to_db();
+
+    #2.组织 查询 参数, (如果为空,不参与查询)
+    $username = isset($_POST['username']) && $_POST['username'] ? $_POST['username'] : '';
+    $postname = isset($_POST['postname']) && $_POST['postname'] ? $_POST['postname'] : '';
+    $postdate = isset($_POST['postdate']) && $_POST['postdate'] ? $_POST['postdate'] : '';
+
+    #3. 根据查询条件组织sql 语句
+    $sql = "select username from bloggers";
+    $where = '';
+    if ($username != '') {
+        $where = " where username = '" . $username . "'";
+    }
+    if ($postname != '') {
+        $where = " where postname = '" . $postname . "'";
+    }
+    if ($postdate != '') {
+        $postdate = date('Y-m-d H:i:s', strtotime($postdate));#时间格式转换成,数据库格式
+        $where = " where postname <= '" . $postdate . "'";
+    }
+    $sql .= $where;
+
+    $res = $pdo->query($sql);//准备查询语句
+    $html = "<table>";
+    foreach ($res as $result){
+        $html .= "<tr class='line'>";
+        $html .=    "<td>" . $result['id'] . "</td>";
+        $html .=    "<td>" . $result['username'] . "</td>";
+        $html .=    "<td>" . $result['password'] . "</td>";
+        $html .=    "<td>" . $result['blogname'] . "</td>";
+        $html .=    "<td>" . $result['postname'] . "</td>";
+        $html .=    "<td>" . $result['postdate'] . "</td>";
+        $html .= "</tr>";
+    }
+    $html .= "</table>";
+
+    echo $html;
+    ?>
+</div>
+
+</body>
+</html>
--- src/Utils/Http.php
+++ src/Utils/Http.php
@@ -55,7 +55,7 @@ class Http
         return $this;
     }
 
-    public function withJson(array $body)
+    public function withJson(string $body)
     {
         $this->json = $body;
         return $this;
